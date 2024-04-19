a7b6d56827045df53ca4afa864a993349e6f168b
Jay Park <sk46624191@gmail.com>
2024-04-19 12:01:46 +0900

    Update alphavantage.py

--- alphavantage.py
+++ alphavantage.py
@@ -7,21 +7,21 @@ def alphavantage_test():
     r_topgainer = requests.get(url_topgainer)
     data_topgainer = r_topgainer.json()
     st.title('Stock Market Updates')
-    st.subheader(data['metadata'])
-    st.write('Last Updated:', data['last_updated'])
+    st.subheader(data_topgainer['metadata'])
+    st.write('Last Updated:', data_topgainer['last_updated'])
 
     # Convert lists of dictionaries to pandas DataFrame and display them
-    if 'top_gainers' in data:
+    if 'top_gainers' in data_topgainer:
         st.subheader('Top Gainers')
-        df_gainers = pd.DataFrame(data['top_gainers'])
+        df_gainers = pd.DataFrame(data_topgainer['top_gainers'])
         st.dataframe(df_gainers.style.hide_index())
 
-    if 'top_losers' in data:
+    if 'top_losers' in data_topgainer:
         st.subheader('Top Losers')
-        df_losers = pd.DataFrame(data['top_losers'])
+        df_losers = pd.DataFrame(data_topgainer['top_losers'])
         st.dataframe(df_losers.style.hide_index())
 
-    if 'most_actively_traded' in data:
+    if 'most_actively_traded' in data_topgainer:
         st.subheader('Most Actively Traded')
-        df_traded = pd.DataFrame(data['most_actively_traded'])
+        df_traded = pd.DataFrame(data_topgainer['most_actively_traded'])
         st.dataframe(df_traded.style.hide_index())
