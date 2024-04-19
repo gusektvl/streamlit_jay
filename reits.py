c5cc48cf08bf2ade489e36380cfe320a849526b8
Jay Park <sk46624191@gmail.com>
2024-04-19 10:02:29 +0900

    Update reits.py

--- reits.py
+++ reits.py
@@ -8,23 +8,41 @@ def news():
     components.html(
         """
         <!-- TradingView Widget BEGIN -->
-<div class="tradingview-widget-container">
-  <div class="tradingview-widget-container__widget"></div>
-  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div>
-  <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-timeline.js" async>
-  {
-  "feedMode": "market",
-  "market": "index",
-  "isTransparent": false,
-  "displayMode": "adaptive",
-  "width": "1200",
-  "height": "2000",
-  "colorTheme": "light",
-  "locale": "en"
-}
-  </script>
-</div>
-<!-- TradingView Widget END -->
+        <div class="tradingview-widget-container">
+          <div class="tradingview-widget-container__widget"></div>
+          <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div>
+          <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-timeline.js" async>
+          {
+          "feedMode": "market",
+          "market": "index",
+          "isTransparent": false,
+          "displayMode": "adaptive",
+          "width": "1200",
+          "height": "1000",
+          "colorTheme": "light",
+          "locale": "en"
+        }
+          </script>
+        </div>
+        <!-- TradingView Widget END -->
+        <!-- TradingView Widget BEGIN -->
+        <div class="tradingview-widget-container">
+          <div class="tradingview-widget-container__widget"></div>
+          <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div>
+          <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-timeline.js" async>
+          {
+          "feedMode": "market",
+          "market": "stock",
+          "isTransparent": false,
+          "displayMode": "adaptive",
+          "width": "1200",
+          "height": "1000",
+          "colorTheme": "light",
+          "locale": "en"
+        }
+          </script>
+        </div>
+        <!-- TradingView Widget END -->
         """,
     width=1200, height=2000
     )
