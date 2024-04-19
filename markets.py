bff1b0f96b8ccba8869064ea27c5e7cf6d0aabaf
Jay Park <sk46624191@gmail.com>
2024-04-19 17:37:31 +0900

    Update markets.py

--- markets.py
+++ markets.py
@@ -2,16 +2,16 @@ import requests
 import streamlit as st
 import streamlit.components.v1 as components
 from newsapi import NewsApiClient
+import pandas as pd
 
 def news():
     url = "https://newsapi.org/v2/top-headlines?country=kr&apiKey=79fc0e8d84134b8e896750585f7192e4"
     r = requests.get(url)
     data = r.json()
-    articles = pd.DataFrame(data['articles'])[['source', 'title', 'description', 'url','publishedAt']]
+    articles = pd.DataFrame(data['articles'])[['source', 'title', 'description', 'url', 'publishedAt']]
     components.html(
         st.dataframe(articles), width=1200, height=2000
     )
-
 
 def sector():
     components.html(
