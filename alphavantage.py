c98d6353439043ade2937c28dafc48c0f4ddf518
Jay Park <sk46624191@gmail.com>
2024-04-19 11:55:14 +0900

    Update alphavantage.py

--- alphavantage.py
+++ alphavantage.py
@@ -1,11 +1,9 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-def alphavantange_test():
+def alphavantage_test():
     st.markdown('test')
     url = 'https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=AAPL&apikey=B8DRZT1OHQPV2UAV'
     r = requests.get(url)
     data = r.json()
-
-    print(data)
-
+    st.markdown(data)
